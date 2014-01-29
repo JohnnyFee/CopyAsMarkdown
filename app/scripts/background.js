3cf5f1a1c7872a1c3c0de33b3bc677e3c3da4b85
JohnnyFee <djohnnyfee@gmail.com>
2014-01-29 13:01:56 +0800

    moidfy extension

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,9 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="red"'
+  });
 });
